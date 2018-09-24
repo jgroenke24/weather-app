38dd53d86ef01ad5ac30121965398e99f8f84860
Jorge <jorge.groenke@gmail.com>
2018-09-24 16:44:56 +0000

    Add functionality to main input and button

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,25 @@ var React = require('react');
 var Header = require('./Header');
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      location: ''
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      location: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.location);
+  }
   render() {
     return (
       <div>
@@ -11,11 +30,16 @@ class Home extends React.Component {
           <h1 className='hero'>
             Enter a City and a State
           </h1>
-          <form className='form-main'>
+          <form 
+            className='form-main'
+            onSubmit={this.handleSubmit}
+          >
             <input
               type='text'
               placeholder='St. George, Utah'
               autoComplete='off'
+              value={this.state.location}
+              onChange={this.handleChange}
             />
             <button
               className='button'
