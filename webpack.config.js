e023e876243a693fe7c40dd1bc8d66d83f7b5b3a
Jorge <jorge.groenke@gmail.com>
2018-09-24 13:57:45 +0000

    Setup dependencies and webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    public: 'webdevbootcamp-jorge-groenke.c9users.io' // That solved it
+  }
+}
